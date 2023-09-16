8b7745d90248bc98c9db10f84eb8df5b7228e24c
Ygor-Machado <ygaounderground011@gmail.com>
2023-09-16 19:00:40 -0300

    cria seeder

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class BrandSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Brand::factory()
+            ->create([
+                'name' => 'Beer and Code'
+            ]);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Feature;
+use App\Models\Product;
+use App\Models\Sku;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +17,15 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Product::factory()
+            ->has(Sku::factory()
+                ->hasAttached(Feature::factory()->count(1), ['value' => '1'])
+                ->count(3)
+            )
+            ->count(5)
+            ->create([
+                'brand_id' => Brand::first()->id,
+                'category_id' => Category::first()->id,
+            ]);
     }
 }
